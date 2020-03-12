783c2b90c60e54873e1228aae2a5b23bac471cc6
Douglass <clementregis1102@gmail.com>
2020-03-12 21:26:03 +0100

    ajout confirmation mdp

--- include/menus.h
+++ include/menus.h
@@ -4,10 +4,10 @@
 typedef struct _menus * Menus;
 
 void monmenus();
-void menu_signin();
+void menu_sign(int);
 void menu_admin();
 void menu_accueil();
-void lire_entier(int *a);
+int lire_entier(int *a);
 int lire_fin_ligne ();
 
 #endif
--- src/Sharethings.c
+++ src/Sharethings.c
@@ -9,6 +9,5 @@ int main (int argc, char *argv[]) {
     menu_accueil();
 
 
-
     return 0;
 }
